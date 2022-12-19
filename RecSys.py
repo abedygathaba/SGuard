14fd0ef6421602f4c8b6c0abfeb59e95b9e09569
abedy Gathaba <48109030+abedygathaba@users.noreply.github.com>
2022-12-19 19:13:12 +0300

    Create RecSys.py

---
+++ RecSys.py
@@ -0,0 +1,57 @@
+Here is an example of a song factorization recommendation system implemented in Python using the Alternating Least Squares (ALS) algorithm:
+
+Chat
+
+import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import norm
+from scipy.sparse.linalg import svds
+
+def song_factorization(user_song_matrix, num_factors=10, regularization=0.01, iterations=20):
+    # Convert the user-song matrix to a scipy sparse matrix
+    user_song_matrix = csr_matrix(user_song_matrix)
+    
+    # Get the dimensions of the matrix
+    num_users, num_songs = user_song_matrix.shape
+    
+    # Initialize the user and song factor matrices with random values
+    user_factors = np.random.random((num_users, num_factors))
+    song_factors = np.random.random((num_songs, num_factors))
+    
+    # Repeat the alternating least squares (ALS) algorithm for a fixed number of iterations
+    for i in range(iterations):
+        # Fix the song factors and solve for the user factors
+        user_factors = solve(user_song_matrix, song_factors, regularization, num_factors)
+        # Fix the user factors and solve for the song factors
+        song_factors = solve(user_song_matrix.T, user_factors, regularization, num_factors)
+        
+    # Return the resulting user and song factor matrices
+    return user_factors, song_factors
+
+def solve(X, Y, regularization, num_factors):
+    # Compute the dot product of the input matrix and the factor matrix
+    XY = X.dot(Y)
+    # Compute the sum of the squared elements of the input matrix
+    X_squared = X.power(2).sum(axis=1)
+    # Compute the dot product of the factor matrix and its transpose
+    Y_squared = Y.T.dot(Y)
+    
+    # Initialize the inverse of Y_squared to a diagonal matrix with the same shape as Y_squared
+    Y_squared_inv = np.eye(Y_squared.shape[0])
+    
+    # Repeat the following loop for each row of the input matrix
+    for i, Xi in enumerate(X):
+        # Extract the non-zero column indices for this row
+        col_indices = Xi.indices
+        # Extract the corresponding values from the factor matrix
+        Yi = Y[col_indices]
+        # Compute the dot product of Yi and its transpose
+        Yi_squared = Yi.T.dot(Yi)
+        # Add the regularization term and the dot product of Yi and its transpose
+        Y_squared_inv += Yi_squared + regularization * np.eye(num_factors)
+        # Compute the dot product of Yi and the corresponding values of the input matrix
+        XYi = Yi.T.dot(Xi.data)
+        # Add the dot product of Yi and the corresponding values of the input matrix
+        Y_squared_inv.dot(XYi, out=XY[i])
+        
+    # Solve
